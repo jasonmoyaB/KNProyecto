04bfbb3ce5b36a8fc096af1825a9470cd5a41fa6
ecalvo92 <ecalvo@crnova.com>
2025-06-03 20:45:00 -0600

    Fin de Semana 4

--- KProyecto/Controllers/HomeController.cs
+++ KProyecto/Controllers/HomeController.cs
@@ -6,19 +6,46 @@ namespace KProyecto.Controllers
     public class HomeController : Controller
     {
         //Abrir las vistas
-        [HttpGet] 
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
         //Ejecutan acciones con botones de tipo submit
-        [HttpPost] 
+        [HttpPost]
         public ActionResult Index(Autenticacion autenticacion)
+        {
+            /**/
+
+            ViewBag.Mensaje = "No se pudo validar su información";
+            return View();
+            //return RedirectToAction("Principal", "Home");
+        }
+
+
+        [HttpGet]
+        public ActionResult Registro()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Registro(Autenticacion autenticacion)
+        {
+            /**/
+
+            ViewBag.Mensaje = "No se pudo registrar su información";
+            return View();
+            //return RedirectToAction("Principal", "Home");
+        }
+
+
+
+
+
+
+        [HttpGet]
         public ActionResult Principal()
         {
             return View();
--- KProyecto/Models/Autenticacion.cs
+++ KProyecto/Models/Autenticacion.cs
@@ -7,6 +7,8 @@ namespace KProyecto.Models
 {
 	public class Autenticacion
 	{
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
         public string NombreUsuario { get; set; }
         public string Contrasenna { get; set; } 
     }
