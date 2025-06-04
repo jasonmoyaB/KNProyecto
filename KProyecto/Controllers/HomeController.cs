8e692c28a1389ade389c2bddb02bc70ecd12e61f
ecalvo92 <ecalvo@crnova.com>
2025-06-03 19:35:46 -0600

    Semana 4

--- KProyecto/Controllers/HomeController.cs
+++ KProyecto/Controllers/HomeController.cs
@@ -1,10 +1,20 @@
-﻿using System.Web.Mvc;
+﻿using KProyecto.Models;
+using System.Web.Mvc;
 
 namespace KProyecto.Controllers
 {
     public class HomeController : Controller
     {
+        //Abrir las vistas
+        [HttpGet] 
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Ejecutan acciones con botones de tipo submit
+        [HttpPost] 
+        public ActionResult Index(Autenticacion autenticacion)
         {
             return View();
         }
---
+++ KProyecto/Models/Autenticacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KProyecto.Models
+{
+	public class Autenticacion
+	{
+        public string NombreUsuario { get; set; }
+        public string Contrasenna { get; set; } 
+    }
+}
