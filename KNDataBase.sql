be53a95e0842aa4cbf19a56d42b9f2220f2f6f99
jason moya brenes <jason.moyabre.es@gmail.com>
2025-06-23 11:26:19 -0600

    Update KNDataBase.sql

--- KNDataBase.sql
+++ KNDataBase.sql
@@ -1,60 +1,65 @@
-USE [master]
-GO
-
-CREATE DATABASE [KNDataBase]
-GO
-
-USE [KNDataBase]
-GO
-
-CREATE TABLE [dbo].[TUsuario](
-	[IdUsuario] [bigint] IDENTITY(1,1) NOT NULL,
-	[Identificacion] [varchar](20) NOT NULL,
-	[Nombre] [varchar](255) NOT NULL,
-	[Correo] [varchar](100) NOT NULL,
-	[Contrasenna] [varchar](10) NOT NULL,
- CONSTRAINT [PK_TUsuario] PRIMARY KEY CLUSTERED 
-(
-	[IdUsuario] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-SET IDENTITY_INSERT [dbo].[TUsuario] ON 
-GO
-INSERT [dbo].[TUsuario] ([IdUsuario], [Identificacion], [Nombre], [Correo], [Contrasenna]) VALUES (2, N'304590415', N'Eduardo Calvo', N'ecalvo90415@ufide.ac.cr', N'90415')
-GO
-SET IDENTITY_INSERT [dbo].[TUsuario] OFF
-GO
-
-CREATE PROCEDURE [dbo].[RegistroUsuario]
-	@Identificacion varchar(20),
-	@Nombre varchar(255),
-	@Correo varchar(100),
-	@Contrasenna varchar(10)
-AS
+IF DB_ID('KNJasonMoya') IS NULL
 BEGIN
-
-	INSERT INTO dbo.TUsuario (Identificacion,Nombre,Correo,Contrasenna)
-	VALUES (@Identificacion, @Nombre, @Correo, @Contrasenna)
-
+    CREATE DATABASE KNJasonMoya;
 END
 GO
 
-CREATE PROCEDURE [dbo].[ValidarInicioSesion]
-	@Correo varchar(100),
-	@Contrasenna varchar(10)
+-- IMPORTANTE: Separar la ejecución para asegurarse de que la base existe antes de usarla
+USE KNJasonMoya;
+GO
+
+-- Borrar tabla si ya existe
+IF OBJECT_ID('dbo.TUsuario', 'U') IS NOT NULL
+DROP TABLE dbo.TUsuario;
+GO
+
+-- Crear tabla TUsuario correctamente
+CREATE TABLE dbo.TUsuario (
+    IdUsuario BIGINT IDENTITY(1,1) NOT NULL,
+    Identificacion VARCHAR(20) NOT NULL,
+    Nombre VARCHAR(255) NOT NULL,
+    Correo VARCHAR(100) NOT NULL,
+    Contrasenna VARCHAR(10) NOT NULL,
+    CONSTRAINT PK_TUsuario PRIMARY KEY CLUSTERED (IdUsuario ASC)
+);
+GO
+
+-- Insertar usuario de prueba
+INSERT INTO dbo.TUsuario (Identificacion, Nombre, Correo, Contrasenna)
+VALUES ('304590415', 'Eduardo Calvo', 'ecalvo90415@ufide.ac.cr', '90415');
+GO
+
+-- Eliminar procedimiento si existe
+IF OBJECT_ID('dbo.RegistroUsuario', 'P') IS NOT NULL
+DROP PROCEDURE dbo.RegistroUsuario;
+GO
+
+-- Crear procedimiento de registro
+CREATE PROCEDURE dbo.RegistroUsuario
+    @Identificacion VARCHAR(20),
+    @Nombre VARCHAR(255),
+    @Correo VARCHAR(100),
+    @Contrasenna VARCHAR(10)
 AS
 BEGIN
-	
-	SELECT	IdUsuario,
-			Identificacion,
-			Nombre,
-			Correo,
-			Contrasenna
-	  FROM	dbo.TUsuario
-	  WHERE Correo = @Correo
-		AND Contrasenna = @Contrasenna
+    INSERT INTO dbo.TUsuario (Identificacion, Nombre, Correo, Contrasenna)
+    VALUES (@Identificacion, @Nombre, @Correo, @Contrasenna);
+END
+GO
 
+-- Eliminar procedimiento si existe
+IF OBJECT_ID('dbo.ValidarInicioSesion', 'P') IS NOT NULL
+DROP PROCEDURE dbo.ValidarInicioSesion;
+GO
+
+-- Crear procedimiento de validación
+CREATE PROCEDURE dbo.ValidarInicioSesion
+    @Correo VARCHAR(100),
+    @Contrasenna VARCHAR(10)
+AS
+BEGIN
+    SELECT IdUsuario, Identificacion, Nombre, Correo, Contrasenna
+    FROM dbo.TUsuario
+    WHERE Correo = @Correo AND Contrasenna = @Contrasenna;
 END
 GO
